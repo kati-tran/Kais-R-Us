b4a47b069eeea2104b975a87e805d305db83212c
jigglequack <45055772+jigglequack@users.noreply.github.com>
2020-05-22 20:36:27 -0700

    moved sqlconnection to storePage.java

--- src/main/java/storePage.java
+++ src/main/java/storePage.java
@@ -6,6 +6,11 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,7 +25,13 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet(urlPatterns = {"/storePage"})
 public class storePage extends HttpServlet {
+    String URL = "jdbc:mysql://localhost:3306/proj2database";
+    String USERNAME = "root";
+    String PASSWORD = "";
 
+    Connection connection = null;
+    PreparedStatement selectProduct = null;
+    ResultSet resultSet = null;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -31,16 +42,50 @@ public class storePage extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            selectProduct = connection.prepareStatement("SELECT * FROM items");
+            
             HttpSession session = request.getSession();
             String var = (String) session.getAttribute("myId");     
             
-            out.println(var);
+            
             RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
             rd.include(request, response); 
+            
+            out.println(var);
+            ResultSet products= selectProduct.executeQuery();
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("</head>" +
+                "<link href=\"css/store.css\" rel=\"stylesheet\" type=\"text/css\">\n" +
+                "<link href=\"css/index.css\" rel=\"stylesheet\" type=\"text/css\">\n" +
+                "</head>"+
+                "<body>" +
+                "<div class=\"main\">" +
+                "<div class=\"row\">");
+            while ( products.next()) {
+                out.println("<div class=\"column\">" +
+                "<div class=\"content\" id=\"" + products.getString("id") + "\" onclick=\"sendData('"+ products.getString("id") + "')\">" +
+                "<img src=\""+products.getString("picture")+"\" style=\"width:100%\">" +
+                " <h4>"+products.getString("name")+"</h4>" +
+                "<p><b>$"+products.getInt("price")+"</b></p>" +
+                "<div class=\"texts\">" +
+                "<p><b>Type: </b>" + products.getString("type") + "&nbsp&nbsp&nbsp<b>Color: </b>" + products.getString("color") + "</p>" +
+                "</div>" +
+                "</div>" +
+                "</div>");
+            }
+            
+            out.println("</div>" + 
+                    "</div>" + 
+                    "</body>" + 
+                    "</html>");
         }
     }
 
--- target/FirstIAE-1.0-SNAPSHOT/index.jsp
+++ target/FirstIAE-1.0-SNAPSHOT/index.jsp
@@ -4,59 +4,12 @@
     Author     : Kati
 --%>
 
-<%@page import="java.sql.*"%>
-<%Class.forName("com.mysql.cj.jdbc.Driver");%>
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-<%!
-    
-    public class Product {
-        String URL = "jdbc:mysql://localhost:3306/proj2database";
-        String USERNAME = "root";
-        String PASSWORD = "";
-
-        Connection connection = null;
-        PreparedStatement selectProduct = null;
-        ResultSet resultSet = null;
-
-        public Product() {
-                
-            try {
-                    
-                    
-                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-                selectProduct = connection.prepareStatement(
-                    "SELECT * FROM items");
-       
-            } catch (Exception e){
-                System.out.println("connection error");
-                e.printStackTrace();
-            }
-        }
-
-        public ResultSet getProducts(){
-
-            try {
-                resultSet = selectProduct.executeQuery();
-            }   catch (SQLException e){
-                e.printStackTrace();
-            }
-            
-            return resultSet;
-        }
-    }
-    %>
-    <%
-        Product product = new Product();
-        ResultSet products = product.getProducts();
-    %>
-    
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 
 </head>
-<div id="poll">
 <link href="css/store.css" rel="stylesheet" type="text/css">
 <link href="css/index.css" rel="stylesheet" type="text/css">
 <body>
@@ -72,25 +25,6 @@
 	  <h1>Kais"R"Us</h1>
 	</div>
     
-
-	<div class="main">
-
-	<!-- Portfolio Gallery Grid -->
-		<div class="row">
-
-			<% while ( products.next()) { %>
-				<div class="column">
-				<div class="content" id="<%= products.getString("id")%>" onclick="sendData('<%= products.getString("id")%>')">
-				<img src="<%= products.getString("picture")%>" style="width:100%">
-                                <h4><%= products.getString("name")%></h4>
-				<p><b>$<%= products.getInt("price")%></b></p>
-				<div class="texts">
-				<p><b>Type: </b><%= products.getString("type")%>&nbsp&nbsp&nbsp<b>Color: </b><%= products.getString("color")%></p>
-				</div>
-				</div>
-				</div>
-                        <% } %>
-
 		<script type="text/javascript">
 			function sendData(id_name) {
 				var xhr= new XMLHttpRequest(); 
@@ -105,10 +39,6 @@
                                   console.log(id_name);
 				  xhr.send("zip=" +id_name);
 			}
-
-		    function replaceIMG(elem){
-		      document.getElementById("image").src = elem;
-		    }
 
 		function zipPredict(str) {
 			var ci = document.getElementById("cityname").value;
@@ -130,13 +60,6 @@
 		}
 
 		</script>
-	
-	</div>
-
-	<!-- END GRID -->
-	</div>
-</div>
-
 
 </body>
 </html>
