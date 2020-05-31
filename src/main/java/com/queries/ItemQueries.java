c91dd1d0a399c2088623323f628cc4e02f90b3f4
Austin Douglas Helmholz <PersonalAccount@Austins-MacBook-Pro-16.local>
2020-05-30 17:48:01 -0700

    Seperated Item queries and added example

---
+++ src/main/java/com/queries/ItemQueries.java
@@ -0,0 +1,58 @@
+package com.queries;
+
+import com.project4.utils.*;
+import com.entities.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ItemQueries {
+
+    public static Item getItemById(String id) throws SQLException, ClassNotFoundException {
+        Database db = new Database();
+        db.openConnection();
+        ResultSet resultSet = db.executQuery("SELECT * FROM items WHERE id=" + id);
+        resultSet.next();
+        Item item = new Item(
+                resultSet.getString("id"),
+                resultSet.getString("picture"),
+                resultSet.getString("picture2"),
+                resultSet.getString("name"),
+                resultSet.getInt("price"),
+                resultSet.getString("type"),
+                resultSet.getString("color"),
+                resultSet.getString("descrip"),
+                resultSet.getString("size")
+        );
+        db.closeConnection();
+        return item;
+    }
+
+
+    public static ArrayList<Item> getAllItems() throws SQLException, ClassNotFoundException {
+        Database db = new Database();
+        db.openConnection();
+        ResultSet resultSet = db.executQuery("SELECT * FROM items");
+        ArrayList<Item> items = new ArrayList<Item>();
+        while(resultSet.next()){
+            items.add(
+                new Item(
+                    resultSet.getString("id"),
+                    resultSet.getString("picture"),
+                    resultSet.getString("picture2"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("price"),
+                    resultSet.getString("type"),
+                    resultSet.getString("color"),
+                    resultSet.getString("descrip"),
+                    resultSet.getString("size")
+                )
+            );
+        }
+        db.closeConnection();
+        return items;
+    }
+
+
+}
