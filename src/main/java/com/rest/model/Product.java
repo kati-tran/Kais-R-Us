cbeba2dd0384bb8dee29748d27651aefe6034c7e
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2020-06-04 05:08:25 -0700

    Created REST Resource, Model, Service

---
+++ src/main/java/com/rest/model/Product.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rest.model;
+
+/**
+ *
+ * @author nguyev12
+ */
+
+public class Product {
+    String id;
+    String picture;
+    String picture2;
+    String name;
+    Integer price;
+    String type;
+    String color;
+    String descrip;
+    String size;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public String getPicture2() {
+        return picture2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public void setPicture2(String picture2) {
+        this.picture2 = picture2;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+  
+}
---
+++ src/main/java/com/rest/resource/ProductResource.java
@@ -0,0 +1,38 @@
+package com.rest.resource;
+
+import com.rest.model.Product;
+import com.rest.service.ProductService;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nguyev12
+ */
+
+@Path("/product")
+public class ProductResource {
+    @Path("{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProductById(@PathParam("id") int id) {
+        Product product = ProductService.getProductById(id);
+        
+        if(product == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+            }
+        
+        return Response.ok(product).build();
+    }
+}
