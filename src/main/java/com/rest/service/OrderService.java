77a272a754105986797e5fd56f18d1c5c962d4c3
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2020-06-07 07:42:21 -0700

    encapsulated sql query with quotes

--- src/main/java/com/rest/service/OrderService.java
+++ src/main/java/com/rest/service/OrderService.java
@@ -20,19 +20,19 @@ public class OrderService {
 
     public static boolean AddOrder(Order order) throws SQLException {
         try {
-            String sql = "INSERT INTO CONFIRM_ORDER" +
-                    "VALUES(" + order.getFirstName() + "," +
-                    order.getLastName() + "," +
-                    order.getPhoneNum() + "," + 
-                    order.getStreet() + "," +
-                    order.getStreet() + "," +
-                    order.getCity() + "," +
-                    order.getState() + "," +
-                    order.getZip() + "," +
-                    order.getShippingMethod() + "," +
-                    order.getCcn() +  "," +
-                    order.getExp() +  "," +
-                    order.getCvv() + ");"; //TODO add shopping cart
+            String sql = "INSERT INTO confirm" +
+                    "VALUES('" + order.getFirstName() + "','" +
+                    order.getLastName() + "','" +
+                    order.getPhoneNum() + "','" + 
+                    order.getStreet() + "','" +
+                    order.getStreet() + "','" +
+                    order.getCity() + "','" +
+                    order.getState() + "','" +
+                    order.getZip() + "','" +
+                    order.getShippingMethod() + "','" +
+                    order.getCcn() +  "','" +
+                    order.getExp() +  "','" +
+                    order.getCvv() + "');"; //TODO add shopping cart
                     
             Class.forName("com.mysql.cj.jdbc.Driver");
             Database database = new Database();
